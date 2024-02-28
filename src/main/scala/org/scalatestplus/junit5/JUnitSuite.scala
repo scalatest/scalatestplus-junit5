e188d9c6d8432b5ca045e0c9aa7db38050f7a459
Jack Lee <38123458+jackhclee@users.noreply.github.com>
2024-02-28 23:22:51 +0000

    Update JUnitSuite.scala
    Update JUnit test example

--- src/main/scala/org/scalatestplus/junit5/JUnitSuite.scala
+++ src/main/scala/org/scalatestplus/junit5/JUnitSuite.scala
@@ -22,33 +22,32 @@ package org.scalatestplus.junit5;
  * methods annotated with <code>Before</code> and <code>After</code>. For example:
  *
  * <pre class="stHighlight">
- * import org.scalatest.junit5.JUnitSuite
+ *
  * import scala.collection.mutable.ListBuffer
- * import _root_.org.junit.Test
- * import _root_.org.junit.Before
  *
  * class TwoSuite extends JUnitSuite {
  *
  *   var sb: StringBuilder = _
  *   var lb: ListBuffer[String] = _
- *
- *   @Before def initialize() {
+ *  
+ *   @BeforeEach def initialize() {
  *     sb = new StringBuilder("ScalaTest is ")
  *     lb = new ListBuffer[String]
  *   }
- *
+ *  
  *   @Test def verifyEasy() {
  *     sb.append("easy!")
  *     assert(sb.toString === "ScalaTest is easy!")
  *     assert(lb.isEmpty)
  *     lb += "sweet"
  *   }
- *
+ *   
  *   @Test def verifyFun() {
  *     sb.append("fun!")
  *     assert(sb.toString === "ScalaTest is fun!")
  *     assert(lb.isEmpty)
  *   }
+ * 
  * }
  * </pre>
  *
