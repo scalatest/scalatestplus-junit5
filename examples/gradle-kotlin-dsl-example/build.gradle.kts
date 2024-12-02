ea97c0c5694f12f3a0a9a929c6e4e7f88a57e670
Chua Chee Seng <cheeseng@amaseng.com>
2024-12-02 14:16:37 +0800

    Updated versions in examples.

--- examples/gradle-kotlin-dsl-example/build.gradle.kts
+++ examples/gradle-kotlin-dsl-example/build.gradle.kts
@@ -9,12 +9,12 @@ repositories {
 }
 
 dependencies {
-    implementation("org.scala-lang:scala3-library_3:3.4.1")
+    implementation("org.scala-lang:scala3-library_3:3.3.4")
 
-    testImplementation("org.scalatest:scalatest_3:3.2.18")
-    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.10.2")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.10.2")
-    testRuntimeOnly("org.scalatestplus:junit-5-10_3:3.2.18.0")
+    testImplementation("org.scalatest:scalatest_3:3.2.19")
+    testRuntimeOnly("org.junit.platform:junit-platform-engine:1.11.3")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.3")
+    testRuntimeOnly("org.scalatestplus:junit-5-11_3:3.2.19.0")
 }
 
 tasks {
